1326f6a9d2ab79e5ff8fab07516926b10bc7a8ab
Juliano Candido <julianosgs@yahoo.com.br>
2024-12-06 08:39:31 -0300

    Implementação ngModel nos inputs

--- src/app/input-email-registro/input-email-registro.component.ts
+++ src/app/input-email-registro/input-email-registro.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-email-registro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input-email-registro.component.html',
   styleUrl: './input-email-registro.component.css'
 })
 export class InputEmailRegistroComponent {
-
+  name:string = '';
 }
--- src/app/input-email/input-email.component.ts
+++ src/app/input-email/input-email.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-email',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input-email.component.html',
   styleUrl: './input-email.component.css'
 })
 export class InputEmailComponent {
-
+  name:string='';
 }
